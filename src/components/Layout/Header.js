cf5d0f88fa29342e09e720114c3d987e7237792a
unknown <antonyvidetta1986@gmail.com>
2023-07-12 15:05:33 +1200

    Completed header section

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './Header.module.css';
+import headerImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>Mealing Ordering System</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={headerImage} alt="A table full of food"/>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Header;
